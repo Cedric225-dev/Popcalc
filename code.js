32c8dc32459dd5d4d30f41b53591676eecdfc618
Cedric225-dev <bloodredmoonbots@gmail.com>
2021-03-25 18:00:33 -0400

    Worked on code for saving input from forms to an array within an object. May need to double check validity

--- code.js
+++ code.js
@@ -1,4 +1,30 @@
-function ProcessInfo(){
+function SubmitInfo(){
+
+  if (!values){
+    var name = document.forms["orderForm"]["Name"].value;
+    var viewcount = document.forms["orderForm"]["Viewcount"].value;
+    var days = document.forms["orderForm"]["Days"].value;
+    var months = document.forms["orderForm"]["Months"].value;
+    var years = document.forms["orderForm"]["Years"].value;
+    var bookmarks = document.forms["orderForm"]["Bookmarks"].value;
+
+    var age = (years * 365) + (months * 30) + days;
+
+    var value = {name, viewcount, age, bookmarks};
+    var values = {value};
+  }else{
+    var name = document.forms["orderForm"]["Name"].value;
+    var viewcount = document.forms["orderForm"]["Viewcount"].value;
+    var days = document.forms["orderForm"]["Days"].value;
+    var months = document.forms["orderForm"]["Months"].value;
+    var years = document.forms["orderForm"]["Years"].value;
+    var bookmarks = document.forms["orderForm"]["Bookmarks"].value;
+
+    var age = (years * 365) + (months * 30) + days;
+
+    var value = {name, viewcount, age, bookmarks};
+    values.push(value);
+  }
 
 
 }
