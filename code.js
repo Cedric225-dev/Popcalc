365f04008adffee30ce494df569f6685e38099a8
Cedric225-dev <bloodredmoonbots@gmail.com>
2021-03-25 18:15:42 -0400

    Made the javascript a bit smarter, rather than two full declarative sets

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 function SubmitInfo(){
 
-  if (!values){
     var name = document.forms["orderForm"]["Name"].value;
     var viewcount = document.forms["orderForm"]["Viewcount"].value;
     var days = document.forms["orderForm"]["Days"].value;
@@ -10,20 +9,18 @@ function SubmitInfo(){
 
     var age = (years * 365) + (months * 30) + days;
 
-    var value = {name, viewcount, age, bookmarks};
-    var values = {value};
-  }else{
-    var name = document.forms["orderForm"]["Name"].value;
-    var viewcount = document.forms["orderForm"]["Viewcount"].value;
-    var days = document.forms["orderForm"]["Days"].value;
-    var months = document.forms["orderForm"]["Months"].value;
-    var years = document.forms["orderForm"]["Years"].value;
-    var bookmarks = document.forms["orderForm"]["Bookmarks"].value;
+    let profile = {
+      "Name" : name,
+      "Viewcount" : viewcount,
+      "Age" : age,
+      "Bookmarks" : bookmarks
+    }
+    if (!profiles){
+      let profiles = {profile}
+    }else{
+      profiles.push(profile);
+    }
 
-    var age = (years * 365) + (months * 30) + days;
-
-    var value = {name, viewcount, age, bookmarks};
-    values.push(value);
   }
 
 
